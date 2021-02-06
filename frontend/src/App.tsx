a47a3986d0f545322547ab21527e96f9fb31d2f0
Tomek Kielar <tomek.kielar00@gmail.com>
2021-02-06 19:23:48 +0100

    added barebones version on pcomponenets placement

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,51 @@
 import React from 'react';
 import './App.css';
+import Post from './components/Post/Post'
+interface AppState {
+  postValue: string
+}
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        Starter Project
-      </header>
-    </div>
-  );
+class App extends React.Component<{},AppState> {
+
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      postValue: "this is a post people can write here"
+    }
+    this.handlePostInputChange = this.handlePostInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  };
+
+  handlePostInputChange(e: any) {
+    this.setState({postValue : e.target.value});
+  }
+  handleSubmit(e: any) {
+    alert('sent')
+  }
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          Starter Project
+        </header>
+        <form onSubmit={this.handleSubmit} className="post-form">
+          <textarea value={this.state.postValue} onChange={this.handlePostInputChange} className="post-text"/>
+          <input type="submit" value="Submit!" className="post-submit"/>
+        </form>
+        <ul className="posts-view-wrapper">
+          <Post author="Typowy Uczeń" creation_time="11:33" postValue="No testujemy sobie tutaj posty zeby dzialy hehehehe XDDDD"></Post>
+          <Post author="Typowy Uczeń 2" creation_time="11:33" postValue="No testujemy sobie tutaj posty zeby dzialy hehehehe XDDDD"></Post>
+          <Post author="Typowy Uczeń 3" creation_time="11:33" postValue="No testujemy sobie tutaj posty zeby dzialy hehehehe XDDDD"></Post>
+          <Post author="Typowy Uczeń 3" creation_time="11:33" postValue="No testujemy sobie tutaj posty zeby dzialy hehehehe XDDDD"></Post>
+          <Post author="Typowy Uczeń 3" creation_time="11:33" postValue="No testujemy sobie tutaj posty zeby dzialy hehehehe XDDDD"></Post>
+          <Post author="Typowy Uczeń 3" creation_time="11:33" postValue="No testujemy sobie tutaj posty zeby dzialy hehehehe XDDDD"></Post>
+          <Post author="Typowy Uczeń 3" creation_time="11:33" postValue="No testujemy sobie tutaj posty zeby dzialy hehehehe XDDDD"></Post>
+          <Post author="Typowy Uczeń 3" creation_time="11:33" postValue="No testujemy sobie tutaj posty zeby dzialy hehehehe XDDDD"></Post>
+        </ul>
+      </div>
+    );
+  }
+
 }
 
 export default App;
---
+++ frontend/src/components/Post/Post.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import './Post.css';
+
+interface PostProps {
+    author: string,
+    creation_time: string
+    postValue: string
+  }
+
+class Post extends React.Component<PostProps, {}> {
+constructor(props: PostProps) {
+    super(props);
+};
+render(){
+    return(
+    <li className="post-wrap">
+        <div className="top-row">
+        <div>{this.props.author}</div>
+        <div>{this.props.creation_time}</div>
+        </div>
+        <div className="middle-row-post">
+        {this.props.postValue}
+        </div>
+        <div className="bottom-row">
+        Tu będą komentarze kiedyś
+        </div>
+    </li>
+    )
+}
+}
+
+export default Post
