00bc0f56d64f0a94c8a5255b616f0ddb7b210d50
Tomek Kielar <tomek.kielar00@gmail.com>
2021-02-06 22:09:33 +0100

    added working user adding & user listing

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,38 @@
 from aiohttp import web
+from aiohttp.web_response import Response
+import json
+from storage import Storage
+
+storageConnection = Storage('users.json','posts.json')
+storageConnection.fetchUserFromJSON()
+userStorage = storageConnection.storage['users']
 
 #template implementation of server
 async def handle(request):
-    name = request.match_info.get('name', "Anonymous")
-    text = "Hello, " + name
-    return web.Response(text=text)
+    return web.json_response(userStorage[0])
+
+async def handleUserID(request):
+    id = int(request.match_info.get('userID', 0))
+    return web.json_response(userStorage[id])
+
+async def handleUsers(request):
+    id = int(request.match_info.get('userID', 0))
+    return web.json_response(userStorage)
+
+async def handleAdding(request):
+    if request.can_read_body:
+        body = await request.json()
+        print(body["first_name"])
+        storageConnection.addUser(body["first_name"],body["second_name"])
+        return web.Response(text=f'User listed below has been added.\n{body}')
+    else:
+        return web.Response(text="Error! Nothing was provided to be added.")
 
 app = web.Application()
 app.add_routes([web.get('/', handle),
-                web.get('/{name}', handle)])
+                web.get('/users/{userID}', handleUserID),
+                web.get('/users', handleUsers),
+                web.post('/add-user', handleAdding)])
 
 if __name__ == '__main__':
     web.run_app(app)
---
+++ backend/storage.py
@@ -0,0 +1,84 @@
+import json
+
+class Storage:
+    storage = {}
+    storage['users'] = []
+    storage['posts'] = []
+    filename_users : str
+    filename_posts : str
+
+    def __init__(self, filename_users: str, filename_posts) -> None:
+        self.filename_users = filename_users
+        self.filename_posts = filename_posts
+
+
+    # USERS MANAGEMENT  PART    
+    def fetchUserFromJSON(self):
+        with open(self.filename_users) as json_file:
+            data = json.load(json_file)
+            for user in data:
+                self.storage['users'].append(user)
+
+    def writeUsersToJSON(self):
+        with open(self.filename_users, 'w') as outfile:
+            json.dump(self.storage['users'], outfile)
+
+    def getUsersMaxId(self) -> int:
+        return len(self.storage['users'])
+
+    def addUser(self, first_name, second_name):
+        temp_usr = {
+            'id' : self.getUsersMaxId()+1,
+            'first_name' : first_name,
+            'second_name' : second_name
+        }
+        self.storage['users'].append(temp_usr)
+        self.writeUsersToJSON()
+
+    def listUsers(self):
+        return self.storage['users']
+
+    # POST MANAGEMENT PART
+
+    def fetchPostsFromJSON(self):
+        with open(self.filename_posts) as json_file:
+            data = json.load(json_file)
+            for post in data:
+                self.storage['posts'].append(post)
+    
+    def writePostsToJSON(self):
+        with open(self.filename_posts, 'w') as outfile:
+            json.dump(self.storage['posts'], outfile)
+    
+    def getPostsMaxId(self) -> int:
+        return len(self.storage['posts'])
+    
+    def addPost(self, author_id, creation_time, post_text):
+        temp_post = {
+            'post_id' : self.getPostsMaxId()+1,
+            'author_id' : author_id,
+            'creation_time' : creation_time,
+            'post_text' : post_text,
+            'comments' : []
+        }
+        self.storage['posts'].append(temp_post)
+        self.writePostsToJSON()
+
+    def listPosts(self):
+        return self.storage['posts']
+
+
+""" Saving to file
+with open('data.json', 'w') as outfile:
+    json.dump(files.userStorage, outfile)
+-----
+with open('data.txt') as json_file:
+    data = json.load(json_file)
+    for p in data['people']:
+        print('Name: ' + p['name'])
+        print('Website: ' + p['website'])
+        print('From: ' + p['from'])
+        print('')
+Opening a file
+print(files.userStorage['users'][3]['second_name'])
+"""
